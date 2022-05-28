f25bb8cabefe50bf4df3179a96b16715aeb5ebf4
Zooce <zooce@hey.com>
2022-05-28 11:59:43 -0700

    add Aggravation plugin + temp hello world system
    I'm just making sure things work so far....

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,24 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(AggravationPlugin)
         .run();
+}
+
+pub struct AggravationPlugin;
+
+impl Plugin for AggravationPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .insert_resource(HelloTimer(Timer::from_seconds(2.0, true)))
+            .add_system(hello_world);
+    }
+}
+
+pub struct HelloTimer(Timer);
+
+fn hello_world(time: Res<Time>, mut timer: ResMut<HelloTimer>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        println!("hello world!");
+    }
 }
