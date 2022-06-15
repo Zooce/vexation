86ed87ea4f5479703326819c3585ad6a880507b8
Zooce <zooce@hey.com>
2022-06-15 01:37:07 -0700

    fix (some) possible moves calculation - BUGS
    There was an obvious issue with the use of `zip` - no idea what I was
    thinking there. There's still a bug where you can jump your own marble
    to get into the center space - left a FIXME comment about it.
    
    The game is in a mostly-playable state!

--- src/system_sets/turn_setup.rs
+++ src/system_sets/turn_setup.rs
@@ -13,49 +13,60 @@ pub fn calc_possible_moves(
     mut state: ResMut<State<GameState>>,
 ) {
     let mut possible_moves = std::collections::BTreeSet::new(); // so we disregard duplicates
+    let dice_values = dice_data.get_dice_values();
     for (entity, marble) in marbles.iter() {
-        for (value, which_die) in dice_data.get_dice_values() { // TODO: need to check if we've already used one or both dice in the previous move
+        for (value, which_die) in dice_values.iter() {
             // exit base / enter board - only one possible move for this marble
             if marble.index == BOARD.len() {
-                if value == 1 {
-                    possible_moves.insert((entity, START_INDEX, which_die));
+                if *value == 1 {
+                    possible_moves.insert((entity, START_INDEX, *which_die)); // path = (BOARD.len(), START_INDEX)
                 }
                 continue;
             }
 
             // exit center space - only one possible move for this marble
             if marble.index == CENTER_INDEX {
-                if value == 1 {
-                    possible_moves.insert((entity, CENTER_EXIT_INDEX, which_die));
+                if *value == 1 {
+                    possible_moves.insert((entity, CENTER_EXIT_INDEX, *which_die)); // path = (CENTER_INDEX, CENTER_EXIT_INDEX)
                 }
                 continue;
             }
 
             // basic move
-            let next_index = marble.index + value as usize;
-            if next_index <= LAST_HOME_INDEX { // the very last home position
-                possible_moves.insert((entity, next_index, which_die));
+            let next_index = marble.index + *value as usize;
+            if next_index <= LAST_HOME_INDEX {
+                possible_moves.insert((entity, next_index, *which_die)); // path = (marble.index..=next_index)
             }
 
             // enter center space
             if CENTER_ENTRANCE_INDEXES.contains(&(next_index - 1)) {
-                possible_moves.insert((entity, CENTER_INDEX, which_die));
+                possible_moves.insert((entity, CENTER_INDEX, *which_die)); // path = (marble.index..next_index) + (CENTER_INDEX)
             }
         }
     }
 
-    // remove possible moves that violate "self-hop" rule per marble
-    for (marble, other_marble) in marbles.iter().zip(marbles.iter()) {
-        if marble.0 == other_marble.0 || marble.1.index > other_marble.1.index {
-            continue;
-        }
-        // remove possible moves where either:
-        // - marble_a lands on marble_b
-        // - marble_b is between marble_a's current position and the destination
-        possible_moves = possible_moves.into_iter().filter(|(_, next_index, _)| {
-            *next_index != other_marble.1.index && !(marble.1.index..*next_index).contains(&other_marble.1.index)
-        }).collect();
-    }
+    println!("TurnSetup - calc_possible_moves: unfiltered = {:?}", possible_moves);
+
+    // FIXME: there's a bug where we can't properly filter out a move that jumps over a marble (same color) and into the center index - we need to know the actual path for this case
+
+    // filter out moves that violate the self-hop rules
+    // - marbles of the same color cannot capture each other
+    // - marbles of the same color cannot jump over each other
+    possible_moves = possible_moves.into_iter().filter(|(entity, dest_index, _)| {
+        let (_, move_marble) = marbles.get(*entity).unwrap();
+        marbles.iter()
+            // no need to compare the same marbles
+            .filter(|(e, _)| *e != *entity)
+            // look for a reason why this is not a valid move
+            .find(|(_, other_marble)| {
+                // not valid if there's already a marble at the destination OR
+                other_marble.index == *dest_index ||
+                // not valid if there's a marble on the way to the destination
+                (move_marble.index < BOARD.len()
+                    && *dest_index != CENTER_INDEX
+                    && (move_marble.index..*dest_index).contains(&other_marble.index))
+            }).is_none()
+    }).collect();
 
     current_player_data.possible_moves = possible_moves;
 
